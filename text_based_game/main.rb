16c73401d9d7f6e82db74fcdece6ee893716ebb5
Kr15k <144447496+Kr15k@users.noreply.github.com>
2024-01-26 09:37:28 +0100

    Update main.rb

--- text_based_game/main.rb
+++ text_based_game/main.rb
@@ -1,22 +1,22 @@
 require 'json'
 
-json_file = File.open "/home/kk/Programming/Ruby/testing/file.jsonc"
+json_file = File.open 'json_path'
 
 @data_from_file = JSON.load json_file
-@cur_room = "underground_room"
+@cur_room = 'underground_room'
 @alive = true
 
 def roomChooser
   @room = @data_from_file[@cur_room]
-  room_name = @room["name"].to_s.split('_').join('').gsub(/(?<=[a-z])(?=[A-Z])/, ' ').upcase
-  room_description = @room["description"]
-  room_items = @room["items"]
+  room_name = @room['name'].to_s.split('_').join('').gsub(/(?<=[a-z])(?=[A-Z])/, ' ').upcase
+  room_description = @room['description']
+  room_items = @room['items']
   puts "\n#{room_name} \n\n#{room_description}"
 end
 
 def roomChecker(input)
-  if @room["exits"].include?(input)
-    @cur_room = @room["exits"][input]
+  if @room['exits'].include?(input)
+    @cur_room = @room['exits'][input]
   else
     puts "\nU died man, wtf"
     @alive = false
@@ -24,10 +24,9 @@ def roomChecker(input)
 end
 
 while true
-  if @alive == true
-    roomChooser
-    roomChecker(gets.chomp)
-  else
-    break
-  end
+  break unless @alive == true
+
+  roomChooser
+  roomChecker(gets.chomp)
+
 end
